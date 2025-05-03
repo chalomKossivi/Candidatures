efb6f3eb6c6c92ad8303cb28e5036bf956738feb
Nguimtsa Momo Loveline <nguimtsaloveline@gmail.com>
2025-05-03 14:45:27 +0200

    header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
+import '../styles/index.css'
 
 const Header = () => {
+
+  const navigate = useNavigate(); // Hook pour changer de page
+
   return (
- <div>Bienvenue veuillez remplir ces informations</div>
-)
+
+    <div className = 'head'>
+
+      <h1>JobTrack</h1>
+
+      <span className ='search' >
+        <input type="text" className = "search-input" placeholder="Rechercher..." />
+        {/* <button class="search-button">🔍</button> */}
+      </span>
+
+      {/* <span className = "menu">
+
+        <button className = "menu-button"> &#9776; </button>
+        <div className = "menu-content">
+            <a href="#">Option 1</a>
+            <a href="#">Option 2</a>
+            <a href="#">Option 3</a>
+        </div>
+        
+      </span> */}
+
+      <button className='NEW' onClick={() => navigate("/Ajouter")}>New</button>
+
+    </div>
+    
+  )
 }
 
 export default Header
